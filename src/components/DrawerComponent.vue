7dad7ef4bc5636ac1ab102e8100fc862b851af90
Your Name <you@example.com>
2026-04-06 19:55:54 -0400

    separacion de los componentes del frame

---
+++ src/components/DrawerComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
+
+const leftDrawerOpen = defineModel<boolean>({ default: false });
+
+interface Props {
+  linksList: EssentialLinkProps[];
+}
+
+const props = defineProps<Props>();
+</script>
+<template>
+  <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <q-list>
+      <q-item-label header> Essential Links </q-item-label>
+
+      <EssentialLink v-for="link in props.linksList" :key="link.title" v-bind="link" />
+    </q-list>
+  </q-drawer>
+</template>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,22 +1,8 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+    <HeaderComponent :toggle-left-drawer="toggleLeftDrawer" />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
-
-        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
-      </q-list>
-    </q-drawer>
+    <DrawerComponent v-model="leftDrawerOpen" :links-list="linksList" />
 
     <q-page-container>
       <router-view />
@@ -26,7 +12,9 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
+import { type EssentialLinkProps } from 'components/EssentialLink.vue';
+import HeaderComponent from 'components/HeaderComponent.vue';
+import DrawerComponent from 'components/DrawerComponent.vue';
 
 const linksList: EssentialLinkProps[] = [
   {
